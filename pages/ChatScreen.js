504efce4af335df8ba551e5e084fa4c51f48e309
Guillaume Bédard <bgui34@gmail.com>
2023-10-05 11:28:17 -0400

    Added next steps

---
+++ pages/ChatScreen.js
@@ -0,0 +1,13 @@
+
+/*
+  *TODO: Fetch the conversation from the store using the ID passed from the parent props
+  *TODO: Add a Appbar with the name in the middle, hamburger menu on the right and a arrow to navigate back on the left
+  *TODO: Create a custom "bubble chat" message component
+  *TODO: Loop over all the messages and render the messages in a flatList (Can render the 20 most recent messages and fetch more if we scroll up)
+  *TODO: Add a text input at the bottom with a send button to the right to send a new message
+  *TODO: Create a backend to actually send messages :) 
+  *
+  */
+function ChatScreen() {
+  // return()
+}
--- pages/MessageScreen.js
+++ pages/MessageScreen.js
@@ -1,6 +1,14 @@
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 
+/*
+ * TODO: Create a store and store some fake conversation with it.
+ * TODO: Create a ScrollableList (FlatList) that will render a custom chat preview component 
+ * (Avatar on left, name as a title and last message under the title)
+ * TODO: Fetch the store conversation, loop over all the conversation and render the chat preview into the flatlist
+ * TODO: When clicking on a chat preview, open a stack navigator to the chatScreen.
+ *
+ */
 export default function MessageScreen() {
   return (
     <SafeAreaView>
